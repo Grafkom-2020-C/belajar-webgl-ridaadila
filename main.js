af6e47c0b1676045462cb6088bdaa2be3222f2b4
ridaadila <ridaadila10@gmail.com>
2020-10-18 16:16:54 +0700

    menggambar garis dengan gl.LINES

--- main.js
+++ main.js
@@ -9,7 +9,10 @@ function main() {
     var vertices = [
         -0.5, 0.5, //titik A
         0.5, 0.5, // titik B
-        0.5, -0.5 // titik C
+        0.5, 0.5, // titik B
+        0.5, -0.5, // titik C
+        0.5, -0.5, // titik C
+        -0.5, 0.5, //titik A
     ];
 
     var vertexBuffer = gl.createBuffer();
@@ -64,9 +67,9 @@ function main() {
     gl.clearColor(0.0,0.22,0.5,1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
 
-    var primitive = gl.POINTS;
+    var primitive = gl.LINES;
     var offset = 0;
-    var nVertex = 3;
+    var nVertex = 6;
     gl.drawArrays(primitive, offset, nVertex);
 
 }
