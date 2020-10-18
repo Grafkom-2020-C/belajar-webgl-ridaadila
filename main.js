c580a6bc1f2a0f0f9955d8bbf267d299759a1df9
ridaadila <ridaadila10@gmail.com>
2020-10-18 16:04:30 +0700

    berhasil menggambar 3titik dengan gl.POINTS

--- main.js
+++ main.js
@@ -2,11 +2,26 @@ function main() {
     var canvas = document.getElementById("myCanvas");
     var gl = canvas.getContext("webgl");
 
+    // Definisi titik pembentuk segitiga
+    // A = (-0.5,0.5)
+    // B = (0.5,0.5)
+    // C = (0.5,-0.5)
+    var vertices = [
+        -0.5, 0.5, //titik A
+        0.5, 0.5, // titik B
+        0.5, -0.5 // titik C
+    ];
+
+    var vertexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
     //ibaratnya di bawah ini adalah .c
     var vertexShaderSource = `
+        attribute vec2 a_Position;
         void main() {
             gl_PointSize = 25.0;
-            gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
+            gl_Position = vec4(a_Position, 0.0, 1.0);
         }
     `;
     var fragmentShaderSource = `
@@ -41,9 +56,17 @@ function main() {
     //memulai menggunakan cat.exe ke dalam konteks  penggambaran grafika
     gl.useProgram(shaderProgram);
 
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    var aPosition = gl.getAttribLocation(shaderProgram, "a_Position");
+    gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(aPosition);
+
     gl.clearColor(0.0,0.22,0.5,1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
 
-    gl.drawArrays(gl.POINTS, 0, 1);
+    var primitive = gl.POINTS;
+    var offset = 0;
+    var nVertex = 3;
+    gl.drawArrays(primitive, offset, nVertex);
 
 }
